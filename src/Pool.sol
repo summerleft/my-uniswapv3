526e1bdc220b90739e66fed1a80011a6eda99d52
summerleft <JunjieHe98@outlook.com>
2024-10-15 23:08:40 +0800

    feat: mint

--- src/Pool.sol
+++ src/Pool.sol
@@ -2,8 +2,12 @@
 pragma solidity ^0.8.13;
 
 import "./lib/Tick.sol";
+import "./lib/Position.sol";
+import "./lib/SafeCast.sol";
+import "./interfaces/IERC20.sol";
 
 contract Pool {
+    using SafeCast for int256;
     address public immutable token0;
     address public immutable token1;
     uint24 public immutable fee;
@@ -17,6 +21,7 @@ contract Pool {
     }
 
     Slot0 public slot0;
+    mapping(bytes32 => Position.Info) public positions;
 
     constructor(
         address _token0,
@@ -33,6 +38,13 @@ contract Pool {
 
     }
 
+    modifier lock() {
+        require(slot0.unlocked, "locked");
+        slot0.unlocked = false;
+        _;
+        slot0.unlocked = true;
+    }
+
     function initialize(uint160 sqrtPriceX96) external {
         require(slot0.sqrtPriceX96 == 0, 'Already initialized');
         int24 tick = TickMath.getTickAtSqrtRatio(sqrtPriceX96);
@@ -41,5 +53,49 @@ contract Pool {
             tick: tick,
             unlocked: true
         });
+    }
+
+    struct ModifyPositionParams {
+        address owner;
+        int24 tickLower;
+        int24 tickUpper;
+        int128 liquidityDelta;
+    }
+
+    function _modifyPosition(ModifyPositionParams memory params)
+        private
+        returns(Position.Info storage position, int256 amount0, int256 amount1)
+    {
+        return (positions[bytes32(0)], 0, 0);
+    }
+
+    function mint(
+        address recipient,
+        int24 tickLower,
+        int24 tickUpper,
+        uint128 amount
+    ) external lock returns (uint256 amount0, uint256 amount1) {
+        require(amount > 0, "amount = 0");
+
+        (, int256 amount0Int, int256 amount1Int) = 
+        _modifyPosition(
+            ModifyPositionParams({
+                owner: recipient,
+                tickLower: tickLower,
+                tickUpper: tickUpper,
+                liquidityDelta: int256(uint256(amount)).toInt128()
+            })
+        );
+
+        amount0 = uint256(amount0Int);
+        amount1 = uint256(amount1Int);
+
+        if (amount0 > 0) {
+            IERC20(token0).transferFrom(msg.sender, address(this), amount0);
+        }
+        if (amount1 > 0) {
+            IERC20(token1).transferFrom(msg.sender, address(this), amount1);
+        }
+
     }
 }
---
+++ src/lib/Position.sol
@@ -0,0 +1,15 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.13;
+
+library Position {
+    struct Info {
+        // the amount of liquidity owned by this position
+        uint128 liquidity;
+        // fee growth per unit of liquidity as of the last update to liquidity or fees owed
+        uint256 feeGrowthInside0LastX128;
+        uint256 feeGrowthInside1LastX128;
+        // the fees owed to the position owner in token0/token1
+        uint128 tokensOwed0;
+        uint128 tokensOwed1;
+    }
+}
