ee39f3274cb682e2b6b8c7654bd0b49e074ef2c5
summerleft <JunjieHe98@outlook.com>
2024-10-15 22:18:35 +0800

    feat: init and Tick

---
+++ src/Pool.sol
@@ -0,0 +1,31 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.13;
+
+import "./lib/Tick.sol";
+
+contract Pool {
+    address public immutable token0;
+    address public immutable token1;
+    uint24 public immutable fee;
+    int24 public immutable tickSpacing;
+    uint128 public immutable maxLiquidityPerTick;
+
+    constructor(
+        address _token0,
+        address _token1,
+        uint24 _fee,
+        int24 _tickSpacing
+    ) {
+        token0 = _token0;
+        token1 = _token1;
+        fee = _fee;
+        tickSpacing = _tickSpacing;
+
+        maxLiquidityPerTick = Tick.tickSpacingToMaxLiquidityPerTick(_tickSpacing);
+
+    }
+
+    function initialize(uint160 sqrtPriceX96) external {
+        
+    }
+}
---
+++ src/lib/Tick.sol
@@ -0,0 +1,13 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.13;
+
+import "./TickMath.sol";
+
+library Tick {
+    function tickSpacingToMaxLiquidityPerTick(int24 tickSpacing) internal pure returns (uint128) {
+        int24 minTick = (TickMath.MIN_TICK / tickSpacing) * tickSpacing;
+        int24 maxTick = (TickMath.MAX_TICK / tickSpacing) * tickSpacing;
+        uint24 numTicks = uint24((maxTick - minTick) / tickSpacing) + 1;
+        return type(uint128).max / numTicks;
+    }
+}
